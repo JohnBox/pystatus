00c7cb55fb05b2f6f0533edb2ee0a9b497574f51
JohnBox <johnboxwon@gmail.com>
2016-01-08 05:32:59 +0200

    Add wifi element

--- pystatus.py
+++ pystatus.py
@@ -1,11 +1,13 @@
-from tools import time
+from tools import time, wifi
 from json import dumps
 from time import sleep
 
 VERSION = {'version': 1}
-b = time.Time('#00AFF0')
+
 print(dumps(VERSION))
 print('[')
 while True:
-    print([b, b], end=',\n')
-    sleep(3)
+    t = time.Time('#00AFF0', '%T')
+    w = wifi.Wifi('#0077B5')
+    print([w, t], end=',\n')
+    sleep(1)
---
+++ tools/wifi.py
@@ -0,0 +1,23 @@
+from .base import Base
+from subprocess import Popen, PIPE
+import re
+import math
+
+
+class Wifi(Base):
+    QUALITIES = ['A', 'B', 'C', 'D', 'E'][::-1]
+
+    def __init__(self, color, iface='wlp2s0'):
+        Base.__init__(self, color)
+        ifconfig = Popen(['ifconfig', iface], stdout=PIPE).stdout.read().decode().rstrip()
+        iwconfig = Popen(['iwconfig', iface], stdout=PIPE).stdout.read().decode().rstrip()
+        rif = re.compile('inet (\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}).*?\((\d+).\d (\w+).*?\((\d+).\d (\w+)', re.I | re.M | re.S)
+        riw = re.compile('ESSID:"(.*?)".*?Quality=(.*?)  Signal', re.I | re.M | re.S)
+        ip, *downup = rif.findall(ifconfig)[0]
+        down = downup[0] + downup[1].replace('i', '')
+        up = downup[2] + downup[3].replace('i', '')
+        ssid, quality = riw.findall(iwconfig)[0]
+        quality = math.floor(eval(quality)*5-1)
+        quality = Wifi.QUALITIES[quality]
+
+        self.full_text = '%s[%s] %s' % (ssid, quality, down)
