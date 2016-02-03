44da5928768ab7fdb3d019f9aca713c5cac7c746
JohnBox <johnboxwon@gmail.com>
2016-02-03 16:14:30 +0200

    Refactor sound, add mixer control namedtuple

--- components/sound.py
+++ components/sound.py
@@ -1,6 +1,6 @@
 from .base import Base
 from subprocess import Popen, call, PIPE, DEVNULL
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
 import re
 
 
@@ -10,9 +10,21 @@ class Sound(Base):
         self.volume_re = re.compile('\[(.*?)\]', re.M)
         if call('amixer', stdout=DEVNULL) == 0:
             self.command = ['amixer', '-c']
+            self.Control = namedtuple('Control', 'volume, dB, state')
         else:
             raise Exception("Not installed amixer")
         self.refresh()
+
+    def _create_control(self, control_str):
+        ctrl = self.volume_re.findall(control_str)
+        ctrl = OrderedDict.fromkeys(ctrl)
+        ctrl = list(ctrl)
+        try:
+            ctrl = self.Control._make(ctrl)
+            ctrl = ctrl._replace(volume=int(ctrl.volume[:-1]))
+            return ctrl
+        except:
+            return
 
     def refresh(self):
         # check plugged usb sound card
@@ -26,7 +38,7 @@ class Sound(Base):
 
         snd = Popen(['amixer', '-c', card_num], stdout=PIPE, stderr=DEVNULL).stdout.read().decode().strip()
         l = [i.strip() for i in re.split("Simple mixer control '(.*?)',0", snd)]
-        d = {k: list(OrderedDict.fromkeys(self.volume_re.findall(v))) for (k, v) in zip(l[1::2], l[2::2])}
+        d = {k: self._create_control(v) for (k, v) in zip(l[1::2], l[2::2])}
         show_icon = self.cfg.get('show_icon', 'False') == 'True'
 
         icon = ''
@@ -34,13 +46,13 @@ class Sound(Base):
             if external:
                 icon = self.cfg.get('external_icon', '')
             else:
-                if '100%' in d['Speaker']:
+                if d['Speaker'].volume > 0:
                     icon = self.cfg.get('speaker_icon', '')
-                elif '100%' in d['Headphone']:
+                elif d['Headphone'].volume > 0:
                     icon = self.cfg.get('headphone_icon', '')
 
-        if 'on' in d[master]:
-            volume = d[master][0][:-1]
+        if d[master].state == 'on':
+            volume = d[master].volume
         else:
             if show_icon:
                 volume = self.cfg.get('mute_icon', '')
