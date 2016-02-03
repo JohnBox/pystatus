9aec778b94a692838e45bd79bd5aa84218691d39
JohnBox <johnboxwon@gmail.com>
2016-02-03 16:51:54 +0200

    Refactor battery, add battery namedtuple

--- components/battery.py
+++ components/battery.py
@@ -1,5 +1,6 @@
 from .base import Base
-from subprocess import Popen, PIPE
+from subprocess import Popen, call, PIPE, DEVNULL
+from collections import namedtuple
 
 import re
 import string
@@ -10,9 +11,26 @@ class Battery(Base):
 
     def __init__(self, cfg):
         super().__init__(cfg)
+        if call('acpi', stdout=DEVNULL, stderr=DEVNULL) != 0:
+            raise Exception('Not installed acpi')
         self.visible = not (cfg.get('show_ac', 'False') == 'True')
         self.ac_re = re.compile('(on|off)')
+        self.Battery = namedtuple('Battery', 'state, capacity, time')
         self.refresh()
+
+    def _create_battery(self, battery_str):
+        battery = [i.strip() for i in re.sub('Battery \d: ', '', battery_str).split(',')]
+        # add empty time if not exist
+        if len(battery) == 2:
+            battery.append('')
+        battery = self.Battery._make(battery)
+        # remove last percent symbol from capacity
+        capacity = int(battery.capacity[:-1])
+        # stay only time
+        time = battery.time.split()
+        time = time[0] if time else ''
+        battery = battery._replace(capacity=capacity, time=time)
+        return battery
 
     def refresh(self):
         self.ac = Popen(['acpi', '-a'], stdout=PIPE).stdout.read().decode().rstrip()
@@ -21,51 +39,29 @@ class Battery(Base):
         show_only_ac = self.cfg.get('show_only_ac', 'False') == 'True'
         if show_only_ac:
             if self.ac == 'off':
-                self.urgent = True
                 self.visible = True
-                self.full_text = ''
             else:
-                self.urgent = False
                 self.visible = False
         else:
             self.battery = Popen('acpi', stdout=PIPE).stdout.read().decode().rstrip()
-            # remove line head and split battery values
-            self.battery = [i.strip() for i in re.sub('Battery \d: ', '', self.battery).split(',')]
-            self.status, self.capacity = self.battery[:2]
+            self.battery = self._create_battery(self.battery)
 
-            # remove last percent symbol
-            self.capacity = int(self.capacity[:-1])
             if self.cfg.get('show_icon', 'False') == 'True':
-                self.capacity //= 20
-                self.capacity -= 1 if self.capacity > 0 else 0
-                self.capacity = Battery.BATTERY[self.capacity]
-
-            self.battery = self.battery[2:]
-            # has time of charging or discharging
-            if self.battery:
-                # stay only time
-                self.time = self.battery[0].split()[0]
-            else:
-                self.time = ''
+                capacity = self.battery.capacity // 20
+                capacity -= 1 if capacity > 0 else 0
+                self.battery = self.battery._replace(capacity=Battery.BATTERY[capacity])
 
             params = {
-                'status': self.status,
-                'capacity': self.capacity,
-                'time': self.time,
+                'state': self.battery.state,
+                'capacity': self.battery.capacity,
+                'time': self.battery.time,
                 'ac': self.ac
             }
 
             self.full_text = self.cfg.get('format', '%(ac)s') % params
 
-            if self.ac == 'off':
-                self.urgent = True
-                self.full_text = ''
-            else:
-                self.urgent = False
-
-
-
-
-
-
-
+        if self.ac == 'off':
+            self.urgent = True
+            self.full_text = ''
+        else:
+            self.urgent = False
