bf7be14b8bf152e9b8e9ae4ecc58816ca94c774d
johnbox <johnboxwon@gmail.com>
2016-07-25 01:57:39 +0300

    Join parser.py and config.py in one file config.py

--- tools/config.py
+++ tools/config.py
@@ -1,9 +1,22 @@
+from argparse import ArgumentParser
 from configparser import ConfigParser
+import os
+import sys
 
-import os.path
 
-
-def config(config_file):
-    _config = ConfigParser()
-    _config.read(os.path.abspath(config_file))
-    return _config if _config else None
+def parse():
+    # default config location pystatus/pystatus.ini
+    default_config_path = os.path.abspath('../pystatus.ini')
+    parser = ArgumentParser(description='Generate status line output for i3bar')
+    parser.add_argument('-c', '--config',
+                        help='path to the config file',
+                        default=default_config_path)
+    args = parser.parse_args()
+    config_path = os.path.abspath(args.config)
+    if os.path.exists(config_path):
+        config = ConfigParser()
+        config.read(config_path)
+        return config
+    else:
+        parser.print_usage(file=sys.stderr)
+        exit(1)
