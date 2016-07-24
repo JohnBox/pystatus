55a6a323e457129fbac13a45b48ab18a45e8a085
johnbox <johnboxwon@gmail.com>
2016-07-25 02:07:53 +0300

    Add not exists message in config parse

--- tools/config.py
+++ tools/config.py
@@ -6,11 +6,10 @@ import sys
 
 def parse():
     # default config location pystatus/pystatus.ini
-    default_config_path = os.path.abspath('../pystatus.ini')
     parser = ArgumentParser(description='Generate status line output for i3bar')
     parser.add_argument('-c', '--config',
                         help='path to the config file',
-                        default=default_config_path)
+                        default='pystatus.ini')
     args = parser.parse_args()
     config_path = os.path.abspath(args.config)
     if os.path.exists(config_path):
@@ -18,5 +17,6 @@ def parse():
         config.read(config_path)
         return config
     else:
+        print('%s not exists' % config_path, file=sys.stderr)
         parser.print_usage(file=sys.stderr)
         exit(1)
