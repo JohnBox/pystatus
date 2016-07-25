b3748b54e02996ba3e590799680b51d77305274f
johnbox <johnboxwon@gmail.com>
2016-07-25 03:46:34 +0300

    Add print all i3bar-protocol fields

--- components/base.py
+++ components/base.py
@@ -5,17 +5,16 @@ class Base:
 
     def __init__(self, cfg):
         self.cfg = cfg
-        self.full_text = '***'
-        self.short_text = '*'
+        self.full_text = cfg.get('full_text', '***')
+        self.short_text = cfg.get('short_text', '*')
         self.color = cfg.get('color', '#EEEEEE')
-        self.background = '#666666'
-        self.border = '#FFFFFF'
-        self.min_width = 10
+        self.background = cfg.get('background', '#111111')
+        self.border = cfg.get('border', '#111111')
+        self.min_width = ' ' * 10
         self.align = 'center'
         self.urgent = False
         self.separator = True
         self.separator_block_width = 9
-        self.markup = 'none'
         self.visible = True
 
     def __str__(self):
@@ -24,7 +23,8 @@ class Base:
             'color',
             'background',
             'border',
-            'urgent',
+            'min_width',
+            'align',
             'separator',
             'separator_block_width'
         ]
